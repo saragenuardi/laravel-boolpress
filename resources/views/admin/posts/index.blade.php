74bba88f6e950151be93e656383e4a6d41a79e81
saragenuardi <saretta.summer16@gmail.com>
2022-07-06 16:04:28 +0200

    implemented index on posts table

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Lista dei tuoi post</h1>
+
+    <div class="row row-cols-3">
+        @foreach ($posts as $post)
+            {{-- single post --}}
+            <div class="col">
+                <div class="card mb-3" style="width: 18rem;">
+                    {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title}}</h5>
+                        {{-- <p class="card-text">{{ $post->}}</p> --}}
+                        <a href="#" class="btn btn-primary">Leggi post</a>
+                    </div>
+                </div>
+            </div>
+            {{-- /single post --}}
+        @endforeach
+    </div>
+@endsection
