0af33d61100d63d2a5939297820a45f51c032966
saragenuardi <saretta.summer16@gmail.com>
2022-07-06 16:20:53 +0200

    add show on table posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title}}</h5>
                         {{-- <p class="card-text">{{ $post->}}</p> --}}
-                        <a href="#" class="btn btn-primary">Leggi post</a>
+                        <a href="{{ route('admin.posts.show', ['post' => $post->id])}}" class="btn btn-primary">Leggi post</a>
                     </div>
                 </div>
             </div>
