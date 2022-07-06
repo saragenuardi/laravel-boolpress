03dca9eaa143174f42c2a3338549fb9a6d86a49a
saragenuardi <saretta.summer16@gmail.com>
2022-07-06 15:40:14 +0200

    Add ,migration and seeder of Posts

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Post;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->title = $faker->sentence();
+            $post->content = $faker->paragraph(rand(10, 30), false);
+            $post->slug = Str::slug($post->title, '-');
+            $post->save();
+        }
+    }
+}
